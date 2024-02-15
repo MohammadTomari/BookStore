e8233ea447deec28119a58317dc868d8b8e80aba
MohammadTomari <>
2024-02-15 09:30:55 +0330

    add: migrations

---
+++ database/migrations/2024_02_14_142005_create_books_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author')->constrained('authors');
+            $table->integer('price');
+            $table->integer('year');
+            $table->foreignId('tag')->constrained('tags');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
